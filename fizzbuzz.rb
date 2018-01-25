4fd1cd08e2a7085f903de8ea5090e3da8b14169a
Christopher Finch <christopher.h.finch@gmail.com>
2018-01-25 08:10:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,9 @@
 def fizzbuzz(int)
-if int%3==0
+if int%15==0
+  "FizzBuzz"
+elsif int%5==0
+  "Buzz"
+elsif int%3==0
   "Fizz"
 end
-
 end
-
-
-
-
-
-#elsif int%5==0
-#puts "Buzz"
-#elsif int%15==0
-#puts "FizzBuzz"
-#end
-#end
